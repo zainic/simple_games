6d07eabd3cbcd35a61a0b97614ddb3c0b8c37301
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-03-09 19:41:50 +0700

    Add background_style function to Playground class
    background_style function creates background based on __init__ function

--- snake_game/main_snake.py
+++ snake_game/main_snake.py
@@ -1,3 +1,7 @@
+from logging import raiseExceptions
+import numpy as np
+import cv2
+
 class Playground:
   """Create area to play the game"""
   def __init__(self, width, height, style):
@@ -5,6 +9,23 @@ class Playground:
     self.width = width
     self.height = height
     self.style = style
+    
+  def background_style(self):
+    """Set backround style"""
+    pixel_background = cv2.imread("texture/" + self.style + ".png") # importing 16 by 16 square image
+    if pixel_background is None:
+      raise AssertionError("Invalid style option") # if the style isn't available
+    """"Create background from 16 by 16 image"""
+    row_background = []
+    for i in range(self.width):
+      for col in pixel_background:
+        row_background.append(col)
+    row_background = cv2.transpose(np.array(row_background))
+    background = []
+    for i in range(self.height):
+      for col in row_background:
+        background.append(col)
+    background = np.array(background)
 
 class Snake(Playground):
   """Create snake and snake's movement"""
