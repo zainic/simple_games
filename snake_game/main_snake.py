37e0b930c403a0280241b4634967f0eac45e09eb
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-03-09 16:08:58 +0700

    Add new classes to main_snake.py script
    Playground : Create area to play the game
    Snake : Create snake and snake's movement
    Food : Create food for snake to grow

--- snake_game/main_snake.py
+++ snake_game/main_snake.py
@@ -1,3 +1,16 @@
+class Playground:
+  """Create area to play the game"""
+  pass
+
+class Snake(Playground):
+  """Create snake and snake's movement"""
+  pass
+
+class Food(Playground):
+  """Create food for snake to grow""" 
+  pass
+
+     
 def main():
   pass
 
