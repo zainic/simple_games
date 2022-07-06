473d586de7512c0e10b8fc8ca8d44584d4727091
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-07-06 23:24:09 +0700

    Ship can now move diagonally

--- war_plane/main.py
+++ war_plane/main.py
@@ -1,14 +1,28 @@
 import numpy as np
 import cv2
 import os, sys
+from pynput import keyboard
 
 from object import *
 from function import *
+
+global pressed_keys
+
+pressed_keys = set()
+
+def on_pressed(key):
+    pressed_keys.add(key)
+
+def on_released(key):
+    pressed_keys.remove(key)
 
 def main():
     background = Background()
     ship = Ship()
     enemy = Enemy()
+    
+    listener = keyboard.Listener(on_press=on_pressed, on_release=on_released)
+    listener.start()
     
     EXIT = False
     
@@ -22,13 +36,14 @@ def main():
         cv2.imshow("WarShip Game", frame)
         key = cv2.waitKey(50) & 0xff
         
-        direction = get_direction_from_key(key)
+        direction = get_direction_from_key(pressed_keys)
         
         ship.move_ship(direction)
         background.move_background(5)
         
         EXIT = get_exit_status(key)
         
+    listenet.stop()
 
 if __name__ == "__main__":
     main()
